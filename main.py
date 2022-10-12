c3491fac760502cd06e84a4293cbd1614c77d7f9
Scott Ruckel <scottruck124@gmail.com>
2022-10-12 16:59:09 -0600

    initial success

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ class clause:
     def __init__(self,argv):
         self.c1 = int(argv[0])
         self.c2 = int(argv[1])
-        self.num1 = abs(self.c1)
-        self.num2 = abs(self.c2)
+        self.num1 = abs(self.c1) - 1
+        self.num2 = abs(self.c2) - 1
         if (self.c1 > 0 and self.c2 > 0):
             self.func = lambda a,b: a or b
         elif (self.c1 > 0):
@@ -16,10 +16,54 @@ class clause:
         else:
             self.func = lambda a,b: not a or not b
 
+def algo(bools,clauses):
+    falses = []
+    falseClauses = {}
+    sat = 0
+    for i in range(len(bools)):
+        falseClauses[i] = 0
+    for i in range(len(clauses)):
+        c = clauses[i]
+        if not c.func(bools[c.num1],bools[c.num2]):
+            falses.append(i)
+            if c.c1 < 0:
+                falseClauses[c.num1] += 1
+            elif c.c2 < 0:
+                falseClauses[c.num2] += 1
+        else:
+            # if c.c1 > 0:
+            #     falseClauses[c.num1] -= 1
+            # if c.c2 > 0:
+            #     falseClauses[c.num2] -= 1
+            sat += 1
+    for i in range(len(bools)):
+        newSat = 0
+        if falseClauses[i] > 0:
+            if bools[i] == 0:
+                bools[i] = 1
+            else:
+                bools[i] = 0
+            for j in range(len(clauses)):
+                a = clauses[j]
+                if a.func(bools[a.num1],bools[a.num2]):
+                    newSat += 1
+            if (newSat > sat):
+                sat = newSat
+            elif bools[i] == 0:
+                bools[i] = 1
+            else:
+                bools[i] = 0
+
+
+    return bools,clauses
+    
+    
+
 # cycle through files
-for file in list(p.rglob("*.txt")):
+for file in list(p.rglob("*63.txt")):
     file = file.__str__().split("/")[-1]
     filename = "inputs/" + file
+    print("Running " + filename + "...")
     groupnum = file[11:14]
     file = open(filename)
     line = file.readline()
@@ -28,24 +72,44 @@ for file in list(p.rglob("*.txt")):
     numInput = int(line[1])
     clauses = {}
     index = 0
-    bools = []
+    bools = [-1 for i in range(numInput)]
     sat = 0
-    for i in range(numInput):
-        bools.append(1)
+    A = open("a.txt",'w')
+    b = open("b.txt",'w')
     for line in file:
         clauses[index] = clause(line.split(" "))
-        a = clauses[index]
-        if a.func(bools[a.num1],bools[a.num2]):
-            sat += 1
         index += 1
+    for i in range(numInput):
+        bools[i] = 1
+    #     c = clauses[i]
+    #     if (c.c1 < 0):
+    #         bools[c.num1] = 0
+    #     elif (c.c1 > 0 and c.c2 < 0):
+    #         bools[c.num2] = 0
+    # for i in range(numInput):
+    #     if (bools[i] == -1):
+    #         bools[i] = 1
+
+    bools,clauses = algo(bools,clauses)
+
+    for i in range(numClauses):
+        a = clauses[i]
+        if a.func(bools[a.num1],bools[a.num2]):
+            b.write(str(bools[a.num1]) + str(bools[a.num2]) + "\n")
+            sat += 1
+        else:
+            A.write(str(bools[a.num1]) + str(bools[a.num2]) + "\n")
     file.close()
+    A.close()
+    b.close()
 
 
     # output to file
     outputFN = "outputs/output_group" + groupnum + ".txt"
     file = open(outputFN,"w")
-    file.write(str(sat))
-    for i in range(numInput):
-        file.write(str(bools[i]))
-    verify(filename,outputFN)
+    file.write(str(sat) + "\n")
+    for i in range(numInput - 1):
+        file.write(str(bools[i]) + "\n")
+    file.write(str(bools[-1]))
     file.close()
+    verify(filename,outputFN)
--- verifier.py
+++ verifier.py
@@ -1,31 +1,35 @@
-import readline
 from signal import valid_signals
+from turtle import numinput
 def verify(s,s1):
     inFile = open(s)
     outFile = open(s1)
     varValues = []
     # Output file
-    
     truesClaimed = int(outFile.readline())
     for line in outFile:
         varValues.append(int(line))
 
     # Input file 
     line = inFile.readline()
-
+    line = line.split(" ")
+    numInput = int(line[0])
     for line in inFile:
+        numInput -= 1
         line = line.split()
         num1 = int(line[0])
         num2 = int(line[1])
-        bool1 = varValues[abs(num1)]
-        bool2 = varValues[abs(num2)]
-        if ((num1 < 0 and not bool1) or
-            (num2 < 0 and not bool2) or
-            (num1 > 0 and bool1) or
-            (num2 > 0 and bool2)):
+        bool1 = varValues[abs(num1) - 1]
+        bool2 = varValues[abs(num2) - 1]
+        if ((num1 <= 0 and not bool1) or
+            (num2 <= 0 and not bool2) or
+            (num1 >= 0 and bool1) or
+            (num2 >= 0 and bool2)):
             truesClaimed -= 1
     if truesClaimed != 0:
-        print("Trues claimed does not match")
+        print(">>>>>>>>>>>Trues claimed does not match +- " + str(truesClaimed))
+        return 0
+    if numInput != 0:
+        print(">>>>>>>>>>>>Incorrect number of lines")
         return 0
     print("Passed verification")
     return 1
